2f6ea0c14a4baff71ddd44c257856b01d9ad8bf5
noomdalv <vladlunasan@gmail.com>
2023-03-04 11:41:25 -0500

    created resolver for posts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
-import { MikroORM, RequestContext } from "@mikro-orm/core";
+import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
 
   await orm.getMigrator().up();
-  await RequestContext.createAsync(orm.em, async () => {});
 
   const app = express();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em.fork() }),
   });
 
   await apolloServer.start();
